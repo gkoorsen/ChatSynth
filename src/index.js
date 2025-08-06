f21b13968c13d10dec3c39e21ccacc4278fabf68
Gerrit Koorsen <g.koorsen@gmail.com>
2025-08-06 11:35:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
 
+Amplify.configure(awsconfig);
 ReactDOM.render(<App />, document.getElementById('root'));
